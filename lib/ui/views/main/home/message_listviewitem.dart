fcdc22bc3bfb805fab4250cdcfa2b49b2fbdb097
Edward Jo <helloedwardjo@gmail.com>
2022-01-10 05:34:59 +0900

    Update padding of message listview item

--- lib/ui/views/main/home/message_listviewitem.dart
+++ lib/ui/views/main/home/message_listviewitem.dart
@@ -12,7 +12,12 @@ class MessageListViewItem extends StatelessWidget {
     return Column(
       children: [
         Container(
-          padding: const EdgeInsets.all(5.0),
+          padding: const EdgeInsets.only(
+            left: 15.0,
+            top: 5.0,
+            bottom: 5.0,
+            right: 10.0,
+          ),
           child: Text(message['message']),
         ),
         const Divider(thickness: 1.0),
