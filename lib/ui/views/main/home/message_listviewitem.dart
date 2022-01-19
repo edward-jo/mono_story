3cc90d8b4c7b9bbe416f56ed88ac85291a27486f
Edward Jo <helloedwardjo@gmail.com>
2022-01-19 13:11:10 +0900

    Beautify message list item

--- lib/ui/views/main/home/message_listviewitem.dart
+++ lib/ui/views/main/home/message_listviewitem.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:mono_story/constants.dart';
 import 'package:mono_story/models/message.dart';
 
 class MessageListViewItem extends StatelessWidget {
@@ -10,19 +11,35 @@ class MessageListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.only(
-            left: 15.0,
-            top: 5.0,
-            bottom: 5.0,
-            right: 10.0,
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 1.0),
+      padding: const EdgeInsets.only(
+        left: 15.0,
+        top: 5.0,
+        bottom: 5.0,
+        right: 10.0,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // -- MESSAGE --
+          Text(message.message, style: Theme.of(context).textTheme.bodyText2),
+
+          // -- DATE TIME --
+          InputChip(
+            backgroundColor: dateTimeBgColor,
+            label: Text(
+              message.createdTime.toString(),
+              style: Theme.of(context).textTheme.caption,
+            ),
+            elevation: 0,
+            onPressed: () {},
           ),
-          child: Text(message.message),
-        ),
-        const Divider(thickness: 1.0),
-      ],
+
+          // -- DIVIDER --
+          const Divider(thickness: 0.5),
+        ],
+      ),
     );
   }
 }
