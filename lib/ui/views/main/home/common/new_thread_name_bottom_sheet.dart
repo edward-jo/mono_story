4adef47cb94b6435123b9e276ac8994a21487b0c
Edward Jo <helloedwardjo@gmail.com>
2022-01-23 19:33:32 +0900

    Add TextField to input new thread name

--- lib/ui/views/main/home/common/new_thread_name_bottom_sheet.dart
+++ lib/ui/views/main/home/common/new_thread_name_bottom_sheet.dart
@@ -1,25 +1,73 @@
+import 'dart:developer' as developer;
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:mono_story/fake_data.dart';
 
-class NewThreadNameBottomSheet extends StatelessWidget {
+class NewThreadNameBottomSheet extends StatefulWidget {
   const NewThreadNameBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  State<NewThreadNameBottomSheet> createState() =>
+      _NewThreadNameBottomSheetState();
+}
+
+class _NewThreadNameBottomSheetState extends State<NewThreadNameBottomSheet> {
+  final _newThreadNameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
 
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        // -- BOTTOM SHEET HEAD --
-        const SizedBox(height: 20),
-        Text(
-          'Select Thread',
-          style: textTheme.headline6!.copyWith(fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: 20),
-      ],
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          // -- BOTTOM SHEET HEAD --
+          const SizedBox(height: 20),
+          Text(
+            'Create New Thread',
+            style: textTheme.headline6!.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+
+          // -- THREAD NAME TEXT FILED --
+          Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Column(
+              children: [
+                TextField(
+                  autofocus: true,
+                  maxLines: 1,
+                  keyboardType: TextInputType.text,
+                  keyboardAppearance: Brightness.light,
+                  controller: _newThreadNameController,
+                ),
+                const SizedBox(height: 10.0),
+
+                // -- DONE BUTTON --
+                TextButton(
+                  onPressed: () => _done(context),
+                  child: const Text('Done'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  void _done(BuildContext context) {
+    final String name = _newThreadNameController.text.trim();
+
+    if (name.isEmpty) return;
+
+    developer.log('New thread name( $name )');
+    // TODO: insert new thread name to database
+
+    Navigator.of(context).pop(name);
   }
 }
