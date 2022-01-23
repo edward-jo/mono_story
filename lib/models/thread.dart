4512b82969d6ebe6b8b7339d8f689b8d1ea388f6
Edward Jo <helloedwardjo@gmail.com>
2022-01-23 19:52:18 +0900

    Add Thread model

---
+++ lib/models/thread.dart
@@ -0,0 +1,17 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:mono_story/constants.dart';
+
+part 'thread.g.dart';
+
+@JsonSerializable()
+class Thread {
+  @JsonKey(name: ThreadsTableCols.id)
+  final int? id;
+  @JsonKey(name: ThreadsTableCols.name)
+  final String name;
+
+  Thread({this.id, required this.name});
+
+  factory Thread.fromJson(Map<String, dynamic> json) => _$ThreadFromJson(json);
+  Map<String, dynamic> toJson() => _$ThreadToJson(this);
+}
