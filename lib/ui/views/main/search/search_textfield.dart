12efbd6a6850c61d919a270f12eaaad3ba2ce24d
Edward Jo <helloedwardjo@gmail.com>
2022-01-27 18:26:51 +0900

    Update search text field UI

--- lib/ui/views/main/search/search_textfield.dart
+++ lib/ui/views/main/search/search_textfield.dart
@@ -1,3 +1,4 @@
+import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
 
 class SearchTextField extends StatefulWidget {
@@ -12,34 +13,50 @@ class _SearchTextFieldState extends State<SearchTextField> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO - Clean up this textfield
+    final textTheme = Theme.of(context).textTheme;
+
     return TextField(
-      style: const TextStyle(fontSize: 16),
       maxLines: 1,
+      style: textTheme.bodyText2,
+      keyboardType: TextInputType.name,
+      keyboardAppearance: Brightness.light,
+      controller: _searchController,
+      onSubmitted: (value) => developer.log('Submitted: $value'),
+      onChanged: (value) => developer.log('onChanged: $value'),
+
+      // -- DECORATION --
       decoration: InputDecoration(
+        // -- COLOR --
+        filled: true,
+        fillColor: Colors.grey.shade200,
+
+        // -- SEARCH HINT --
+        hintText: 'Search',
+        hintStyle: textTheme.bodyText2?.copyWith(color: Colors.grey),
+
+        // -- SEARCH ICON --
         prefixIcon: const Icon(Icons.search, color: Colors.grey),
+
+        // -- CANCEL ICON BUTTON --
         suffixIcon: IconButton(
           icon: const Icon(Icons.cancel, color: Colors.grey),
-          onPressed: () => print('Cancel Icon pressed'),
+          onPressed: () => _searchController.clear(),
         ),
+
+        // -- PADDING --
         contentPadding: const EdgeInsets.symmetric(vertical: 3),
+
+        // -- OUTLINE INPUT BORDER --
         border: const OutlineInputBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(5.0),
-          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
         enabledBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey),
+          borderSide: BorderSide.none,
         ),
         focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey),
+          borderSide: BorderSide.none,
         ),
       ),
-      cursorColor: Colors.grey,
-      keyboardType: TextInputType.name,
-      controller: _searchController,
-      onSubmitted: null,
-      onChanged: (value) => print('onChanged: $value'),
     );
   }
 }
