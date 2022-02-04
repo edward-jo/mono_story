f578bfede8696410b06816836b029497a379ece3
Edward Jo <helloedwardjo@gmail.com>
2022-02-04 16:08:37 +0900

    Add MessageSearchResultListView class

---
+++ lib/ui/views/main/home/message_search_result_listview.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:mono_story/models/message.dart';
+import 'package:mono_story/ui/views/main/home/message_listviewitem.dart';
+import 'package:mono_story/view_models/message_viewmodel.dart';
+import 'package:provider/src/provider.dart';
+
+class MessageSearchResultListView extends StatefulWidget {
+  const MessageSearchResultListView({
+    Key? key,
+    required this.query,
+  }) : super(key: key);
+
+  final String query;
+
+  @override
+  State<MessageSearchResultListView> createState() =>
+      _MessageSearchResultListViewState();
+}
+
+class _MessageSearchResultListViewState
+    extends State<MessageSearchResultListView> {
+  late final MessageViewModel _messageVM;
+
+  @override
+  void initState() {
+    super.initState();
+    _messageVM = context.read<MessageViewModel>();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Message> messageList;
+    messageList = context.watch<MessageViewModel>().messages;
+
+    final searchResult = messageList
+        .where(
+          (e) => e.message.toLowerCase().contains(widget.query.toLowerCase()),
+        )
+        .toList();
+
+    return Column(
+      children: [
+        const SizedBox(height: 10.0),
+        Expanded(
+          child: SizedBox(
+            child: Scrollbar(
+              // controller: _scrollController,
+              child: ListView.separated(
+                // controller: _scrollController,
+                itemCount: searchResult.length,
+                itemBuilder: (_, i) {
+                  return MessageListViewItem(
+                    message: searchResult[i],
+                    onStar: () {},
+                    onDelete: () async {
+                      _messageVM.deleteMessage(i);
+                    },
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return const Divider(thickness: 0.5);
+                },
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
