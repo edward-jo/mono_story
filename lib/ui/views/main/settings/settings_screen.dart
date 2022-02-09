40a62cf48c92b22f74c8dff5dda4af0e011c7b38
Edward Jo <helloedwardjo@gmail.com>
2022-02-10 06:37:22 +0900

    Show ListTiles on Settings screen

--- lib/ui/views/main/settings/settings_screen.dart
+++ lib/ui/views/main/settings/settings_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mono_story/ui/common/platform_widget.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -15,8 +17,58 @@ class _SettingsScreenState extends State<SettingsScreen> {
         automaticallyImplyLeading: false,
         title: const Text('Settings'),
       ),
-      body: const Center(
-        child: Text('Settings Screen'),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            // THREADS
+            ListTile(
+              leading: const PlatformWidget(
+                cupertino: Icon(CupertinoIcons.tag),
+                material: Icon(Icons.tag),
+              ),
+              title: const Text('Threads'),
+              trailing: const PlatformWidget(
+                cupertino: Icon(CupertinoIcons.chevron_forward, size: 20.0),
+                material: Icon(Icons.chevron_right, size: 20.0),
+              ),
+              onTap: () {},
+            ),
+
+            // BACK UP
+            ListTile(
+              leading: const PlatformWidget(
+                cupertino: Icon(CupertinoIcons.cloud_upload),
+                material: Icon(Icons.cloud_upload),
+              ),
+              title: const Text('Back up'),
+              onTap: () {},
+            ),
+
+            // RESTORE
+            ListTile(
+              leading: const PlatformWidget(
+                cupertino: Icon(CupertinoIcons.cloud_download),
+                material: Icon(Icons.cloud_download),
+              ),
+              title: const Text('Restore'),
+              onTap: () {},
+            ),
+
+            // ABOUT
+            ListTile(
+              leading: const PlatformWidget(
+                cupertino: Icon(CupertinoIcons.info),
+                material: Icon(Icons.info),
+              ),
+              title: const Text('About'),
+              trailing: const PlatformWidget(
+                cupertino: Icon(CupertinoIcons.chevron_forward, size: 20.0),
+                material: Icon(Icons.chevron_right, size: 20.0),
+              ),
+              onTap: () {},
+            )
+          ],
+        ),
       ),
     );
   }
