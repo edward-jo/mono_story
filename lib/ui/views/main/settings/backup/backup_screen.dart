4fccc8f371330190e9411892bbbe43703d5db074
Edward Jo <helloedwardjo@gmail.com>
2022-02-16 16:43:50 +0900

    Add Cancel Backup ListTile

--- lib/ui/views/main/settings/backup/backup_screen.dart
+++ lib/ui/views/main/settings/backup/backup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mono_story/ui/common/mono_divider.dart';
 import 'package:mono_story/ui/common/platform_switch.dart';
 import 'package:mono_story/ui/common/platform_widget_base.dart';
 
@@ -14,6 +15,7 @@ class BackupScreen extends StatefulWidget {
 
 class _BackupScreenState extends State<BackupScreen> {
   bool _useCellularData = false;
+  bool _isBackingUp = false;
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +27,7 @@ class _BackupScreenState extends State<BackupScreen> {
       body: Center(
           child: Column(
         children: <Widget>[
+          // USE CELLULAR DATA
           ListTile(
             title: const Text('Use Cellular Data'),
             trailing: PlatformSwitch(
@@ -34,28 +37,103 @@ class _BackupScreenState extends State<BackupScreen> {
               },
             ),
           ),
-          const SizedBox(height: 20),
-          ListTile(
-            title: Text(
-              'Back Up Now',
-              style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                    color: Colors.blue,
-                  ),
-            ),
-            subtitle: Container(
-              padding: const EdgeInsets.only(top: 10),
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Last successful backup: yesterday 2:00 PM',
-                style: Theme.of(context).textTheme.caption?.copyWith(
-                      fontSize: 12,
-                    ),
-              ),
-            ),
-            onTap: () {},
-          ),
+
+          //
+          const MonoDivider(height: 10, color: Colors.black),
+
+          // BACK UP / CANCEL BACK UP
+          _isBackingUp
+              ? _CancelBackupWidget(
+                  onTap: () => setState(() {
+                    _isBackingUp = false;
+                  }),
+                )
+              : _BackUpNowWidget(
+                  wifiRequired: !_useCellularData,
+                  onTap: () => setState(() {
+                    _isBackingUp = true;
+                  }),
+                ),
         ],
       )),
+    );
+  }
+}
+
+class _BackUpNowWidget extends StatelessWidget {
+  const _BackUpNowWidget({
+    Key? key,
+    required this.wifiRequired,
+    required this.onTap,
+  }) : super(key: key);
+
+  final bool wifiRequired;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).textTheme.bodyText1?.copyWith(
+          color: Colors.blue,
+        );
+    final subtitleStyle = Theme.of(context).textTheme.caption?.copyWith(
+          fontSize: 13,
+        );
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        // BACK UP NOW
+        ListTile(
+          title: Text('Back Up Now', style: titleStyle),
+          onTap: onTap,
+        ),
+
+        //
+        const MonoDivider(height: 10, color: Colors.black),
+
+        // SUBTITLES
+        Container(
+          padding: const EdgeInsets.only(left: 20, top: 10),
+          alignment: Alignment.centerLeft,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // MESSAGE: WIFI REQUIRED
+              if (wifiRequired)
+                Text(
+                  'You must be connected to Wi-Fi network to start a backup.',
+                  style: subtitleStyle,
+                ),
+              const SizedBox(height: 10),
+
+              // MESSAGE: LAST SUCCESSFUL BACKUP TIME
+              Text(
+                'Last successful backup: yesterday 2:00 PM',
+                style: subtitleStyle,
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _CancelBackupWidget extends StatelessWidget {
+  const _CancelBackupWidget({Key? key, required this.onTap}) : super(key: key);
+
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        'Cancel Backup',
+        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+              color: Colors.blue,
+            ),
+      ),
+      onTap: onTap,
     );
   }
 }
