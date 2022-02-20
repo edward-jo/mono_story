849010fcd76984355ef00adf37a796db7f27897f
Edward Jo <helloedwardjo@gmail.com>
2022-02-20 15:36:25 +0900

    Add Settings Model

---
+++ lib/models/settings.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'settings.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Settings _$SettingsFromJson(Map<String, dynamic> json) => Settings(
+      useCellularData: json['use_cellular_data'] as bool?,
+    );
+
+Map<String, dynamic> _$SettingsToJson(Settings instance) => <String, dynamic>{
+      'use_cellular_data': instance.useCellularData,
+    };
